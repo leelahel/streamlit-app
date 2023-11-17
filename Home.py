0da04cbad1dabfa49a53074b8ecea0c0519a5a72
이정찬 <136438463+leelahel@users.noreply.github.com>
2023-11-17 17:48:30 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,177 +5,208 @@ from streamlit_option_menu import option_menu
 from numerize.numerize import numerize
 import time
 from streamlit_extras.metric_cards import style_metric_cards
+st.set_option('deprecation.showPyplotGlobalUse', False)
 import plotly.graph_objs as go
 
-# Uncomment this line if you use mysql
-# from query import *
+#uncomment this line if you use mysql
+#from query import *
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_page_config(page_title="Dashboard",page_icon="🌍",layout="wide")
+st.header("DESCRIPTIVE ANALYTICS DASHBOARD | INSURANCE KPI  &  TRENDS ")
 
-# Load Style css
-with open('style.css') as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+#all graphs we use custom css not streamlit 
+theme_plotly = None 
 
-# Uncomment these two lines if you fetch data from mysql
-# result = view_all_data()
-# df = pd.DataFrame(result, columns=["Policy","Expiry","Location","State","Region","Investment","Construction","BusinessType","Earthquake","Flood","Rating","id"])
 
-# Load excel file | comment this line when you fetch data from mysql
-df = pd.read_excel('data.xlsx', sheet_name='Sheet1')
+# load Style css
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
-# Sidebar Logo
-region = st.sidebar.multiselect(
+#uncomment these two lines if you fetch data from mysql
+#result = view_all_data()
+#df=pd.DataFrame(result,columns=["Policy","Expiry","Location","State","Region","Investment","Construction","BusinessType","Earthquake","Flood","Rating","id"])
+
+#load excel file | comment this line when  you fetch data from mysql
+df=pd.read_excel('data.xlsx', sheet_name='Sheet1')
+
+#side bar logo
+
+
+#switcher
+
+region=st.sidebar.multiselect(
     "SELECT REGION",
-    options=df["Region"].unique(),
-    default=df["Region"].unique(),
+     options=df["Region"].unique(),
+     default=df["Region"].unique(),
 )
-location = st.sidebar.multiselect(
+location=st.sidebar.multiselect(
     "SELECT LOCATION",
-    options=df["Location"].unique(),
-    default=df["Location"].unique(),
+     options=df["Location"].unique(),
+     default=df["Location"].unique(),
 )
-construction = st.sidebar.multiselect(
+construction=st.sidebar.multiselect(
     "SELECT CONSTRUCTION",
-    options=df["Construction"].unique(),
-    default=df["Construction"].unique(),
+     options=df["Construction"].unique(),
+     default=df["Construction"].unique(),
 )
 
-df_selection = df.query(
+df_selection=df.query(
     "Region==@region & Location==@location & Construction ==@construction"
 )
 
-# Function for basic descriptive analytics like Mean, Mode, Sum, etc.
+#this function performs basic descriptive analytics like Mean,Mode,Sum  etc
 def Home():
     with st.expander("VIEW EXCEL DATASET"):
-        showData = st.multiselect('Filter: ', df_selection.columns, default=["Policy", "Expiry", "Location", "State", "Region", "Investment", "Construction", "BusinessType", "Earthquake", "Flood", "Rating"])
-        st.dataframe(df_selection[showData], use_container_width=True)
-    
-    # Compute top analytics
+        showData=st.multiselect('Filter: ',df_selection.columns,default=["Policy","Expiry","Location","State","Region","Investment","Construction","BusinessType","Earthquake","Flood","Rating"])
+        st.dataframe(df_selection[showData],use_container_width=True)
+    #compute top analytics
     total_investment = float(df_selection['Investment'].sum())
     investment_mode = float(df_selection['Investment'].mode())
     investment_mean = float(df_selection['Investment'].mean())
-    investment_median = float(df_selection['Investment'].median())
+    investment_median= float(df_selection['Investment'].median()) 
     rating = float(df_selection['Rating'].sum())
 
-    total1, total2, total3, total4, total5 = st.columns(5, gap='small')
+    total1,total2,total3,total4,total5=st.columns(5,gap='small')
     with total1:
-        st.info('Total Sum Investment', icon="💰")
-        st.metric(label="Sum TZS", value=f"{total_investment:,.0f}")
+        st.info('Total Sum Investment',icon="💰")
+        st.metric(label="Sum TZS",value=f"{total_investment:,.0f}")
 
     with total2:
-        st.info('Most Frequent Investment', icon="💰")
-        st.metric(label="Mode TZS", value=f"{investment_mode:,.0f}")
+        st.info('Most Frequent Investment',icon="💰")
+        st.metric(label="Mode TZS",value=f"{investment_mode:,.0f}")
 
     with total3:
-        st.info('Average Mean', icon="💰")
-        st.metric(label="Average TZS", value=f"{investment_mean:,.0f}")
+        st.info('Average Mean',icon="💰")
+        st.metric(label="Average TZS",value=f"{investment_mean:,.0f}")
 
     with total4:
-        st.info('Central Earnings', icon="💰")
-        st.metric(label="Median TZS", value=f"{investment_median:,.0f}")
+        st.info('Central Earnings',icon="💰")
+        st.metric(label="Median TZS",value=f"{investment_median:,.0f}")
 
     with total5:
-        st.info('Ratings', icon="💰")
-        st.metric(label="Rating", value=numerize(rating), help=f""" Total Rating: {rating} """)
-    style_metric_cards(background_color="#FFFFFF", border_left_color="#686664", border_color="#000000", box_shadow="#F71938")
+        st.info('Ratings',icon="💰")
+        st.metric(label="Rating",value=numerize(rating),help=f""" Total Rating: {rating} """)
+    style_metric_cards(background_color="#FFFFFF",border_left_color="#686664",border_color="#000000",box_shadow="#F71938")
 
-    # Variable distribution Histogram
+    #variable distribution Histogram
     with st.expander("EXPLORATORY VARIABLE DISTRIBUTIONS BY FREQUENCY"):
-        df.hist(figsize=(16, 8), color='#898784', zorder=2, rwidth=0.9, legend=['Investment'])
-        st.pyplot()
+     df.hist(figsize=(16,8),color='#898784', zorder=2, rwidth=0.9,legend = ['Investment']);
+     st.pyplot()
 
-# Graphs
+   
+
+#graphs
 def graphs():
-    # Simple bar graph investment by business type
-    investment_by_business_type = (
+    #total_investment=int(df_selection["Investment"]).sum()
+    #averageRating=int(round(df_selection["Rating"]).mean(),2) 
+    #simple bar graph  investment by business type
+    investment_by_business_type=(
         df_selection.groupby(by=["BusinessType"]).count()[["Investment"]].sort_values(by="Investment")
     )
-    fig_investment = px.bar(
-        investment_by_business_type,
-        x="Investment",
-        y=investment_by_business_type.index,
-        orientation="h",
-        title="<b> INVESTMENT BY BUSINESS TYPE </b>",
-        color_discrete_sequence=["#0083B8"] * len(investment_by_business_type),
-        template="plotly_white",
+    fig_investment=px.bar(
+       investment_by_business_type,
+       x="Investment",
+       y=investment_by_business_type.index,
+       orientation="h",
+       title="<b> INVESTMENT BY BUSINESS TYPE </b>",
+       color_discrete_sequence=["#0083B8"]*len(investment_by_business_type),
+       template="plotly_white",
     )
     fig_investment.update_layout(
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(color="black"),
-        xaxis=(dict(showgrid=True))
-    )
+     plot_bgcolor="rgba(0,0,0,0)",
+     font=dict(color="black"),  
+    xaxis=(dict(showgrid=True))
+     )
 
-    # Simple line graph investment by state
-    investment_state = df_selection.groupby(by=["State"]).count()[["Investment"]]
-    fig_state = px.line(
-        investment_state,
-        x=investment_state.index,
-        y="Investment",
-        orientation="v",
-        title="<b> INVESTMENT BY STATE </b>",
-        color_discrete_sequence=["#0083b8"] * len(investment_state),
-        template="plotly_white",
+    #simple line graph investment by state
+    investment_state=df_selection.groupby(by=["State"]).count()[["Investment"]]
+    fig_state=px.line(
+       investment_state,
+       x=investment_state.index,
+       y="Investment",
+       orientation="v",
+       title="<b> INVESTMENT BY STATE </b>",
+       color_discrete_sequence=["#0083b8"]*len(investment_state),
+       template="plotly_white",
     )
     fig_state.update_layout(
-        xaxis=dict(tickmode="linear"),
-        plot_bgcolor="rgba(0,0,0,0)",
-        yaxis=(dict(showgrid=False))
-    )
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False))
+     )
 
-    left, right, center = st.columns(3)
-    left.plotly_chart(fig_state, use_container_width=True)
-    right.plotly_chart(fig_investment, use_container_width=True)
-
+    left,right,center=st.columns(3)
+    left.plotly_chart(fig_state,use_container_width=True)
+    right.plotly_chart(fig_investment,use_container_width=True)
+    
     with center:
-        # Pie chart
-        fig = px.pie(df_selection, values='Rating', names='State', title='RATINGS BY REGIONS')
-        fig.update_layout(legend_title="Regions", legend_y=0.9)
-        fig.update_traces(textinfo='percent+label', textposition='inside')
-        st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+      #pie chart
+      fig = px.pie(df_selection, values='Rating', names='State', title='RATINGS BY REGIONS')
+      fig.update_layout(legend_title="Regions", legend_y=0.9)
+      fig.update_traces(textinfo='percent+label', textposition='inside')
+      st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
-# Function to show current earnings against expected target
+#function to show current earnings against expected target     
 def Progressbar():
-    st.markdown("""<style>.stProgress > div > div > div > div { background-image: linear-gradient(to right, #99ff99 , #FFFF00)}</style>""", unsafe_allow_html=True,)
-    target = 3000000000
-    current = df_selection["Investment"].sum()
-    percent = round((current / target * 100))
-    mybar = st.progress(0)
+    st.markdown("""<style>.stProgress > div > div > div > div { background-image: linear-gradient(to right, #99ff99 , #FFFF00)}</style>""",unsafe_allow_html=True,)
+    target=3000000000
+    current=df_selection["Investment"].sum()
+    percent=round((current/target*100))
+    mybar=st.progress(0)
 
-    if percent > 100:
+    if percent>100:
         st.subheader("Target done !")
     else:
-        st.write("you have ", percent, "% ", "of ", (format(target, 'd')), "TZS")
-        for percent_complete in range(percent):
-            time.sleep(0.1)
-            mybar.progress(percent_complete + 1, text=" Target Percentage")
+     st.write("you have ",percent, "% " ,"of ", (format(target, 'd')), "TZS")
+     for percent_complete in range(percent):
+        time.sleep(0.1)
+        mybar.progress(percent_complete+1,text=" Target Percentage")
 
-# Sidebar Menu
+#menu bar
 def sideBar():
-    with st.sidebar:
-        st.button("🏷 CAMERA", key="camera_btn")  # Add a new button
-        selected = option_menu(
-            menu_title="Main Menu",
-            options=["Home", "Progress"],
-            icons=["house", "eye"],
-            menu_icon="cast",
-            default_index=0
-        )
-    if selected == "Home":
-        Home()
-        graphs()
-    if selected == "Progress":
-        Progressbar()
-        graphs()
-
-# Define action when the "CAMERA" button is clicked
-if st.sidebar.button("🏷 CAMERA", key="camera_btn"):
-    st.write("You clicked the CAMERA button! Add your camera-related content here.")
+ with st.sidebar:
+    selected=option_menu(
+        menu_title="Main Menu",
+        options=["Home","Progress"],
+        icons=["house","eye"],
+        menu_icon="cast",
+        default_index=0
+    )
+ if selected=="Home":
+    #st.subheader(f"Page: {selected}")
+    Home()
+    graphs()
+ if selected=="Progress":
+    #st.subheader(f"Page: {selected}")
+    Progressbar()
+    graphs()
 
 sideBar()
-st.sidebar.image("data/logo1.png", caption="")
+st.sidebar.image("data/logo1.png",caption="")
 
-# Theme
-hide_st_style = """ 
+
+st.subheader('PICK FEATURES TO EXPLORE DISTRIBUTIONS TRENDS BY QUARTILES',)
+#feature_x = st.selectbox('Select feature for x Qualitative data', df_selection.select_dtypes("object").columns)
+feature_y = st.selectbox('Select feature for y Quantitative Data', df_selection.select_dtypes("number").columns)
+fig2 = go.Figure(
+    data=[go.Box(x=df['BusinessType'], y=df[feature_y])],
+    layout=go.Layout(
+        title=go.layout.Title(text="BUSINESS TYPE BY QUARTILES OF INVESTMENT"),
+        plot_bgcolor='rgba(0, 0, 0, 0)',  # Set plot background color to transparent
+        paper_bgcolor='rgba(0, 0, 0, 0)',  # Set paper background color to transparent
+        xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color
+        yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color
+        font=dict(color='#cecdcd'),  # Set text color to black
+    )
+)
+# Display the Plotly figure using Streamlit
+st.plotly_chart(fig2,use_container_width=True)
+
+
+
+#theme
+hide_st_style=""" 
+
 <style>
 #MainMenu {visibility:hidden;}
 footer {visibility:hidden;}
